31392a4fd725ef09a2d5ff7a5dd6aabaf289653a
Lucas Brandão <71549267+correlucas@users.noreply.github.com>
2023-01-17 21:59:52 +0100

    accordeon opening

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const question = document.querySelectorAll('dt');
+const answer = document.querySelectorAll('dd');
+const arrow = document.querySelectorAll('dt:after')
+
+
+question[0].classList.toggle('active');
+answer[0].classList.toggle('active');
+
+function activeAccordion() {
+  answer.forEach((answers) => {
+    answers.classList.remove('active') //remove from all answers the active
+  });
+
+  this.classList.toggle('active'); //add active when the selected item is clicked
+  this.nextElementSibling.classList.toggle('active'); //add active when the previous item is clicked (the question)
+}
+
+question.forEach((item) => {
+  item.addEventListener('click', activeAccordion)
+});
+
